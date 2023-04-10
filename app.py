48cd2c6e24b2586a5390b82dbcf581437bde6a13
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-04-11 02:16:40 +0530

    converting to flask

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,jsonify,request
+# from database import load_jobs_from_db,load_job_from_db, add_application_to_db
+
+
+print("yo")
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+ 
+  return  render_template('home.html')
+
+# @app.route("/job/<id>")
+# def show_job(id):
+#   job = load_job_from_db(id)
+#   if not job:
+#     return "No job found", 404 
+#   return render_template('jobpage.html',job=job)
+#   #return jsonify(job)
+
+
+# @app.route("/job/<id>/apply", methods=['post'])
+# def apply_job(id):
+#    data = request.form
+#    job = load_job_from_db(id)
+#    add_application_to_db(data)
+#    #return jsonify(data)
+#    return render_template('submitted_form.html',application=data)
+
+
+#print(__name__)
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug = True)
+  
+
+
+
+
+
+
