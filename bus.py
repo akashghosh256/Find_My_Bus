c53c5da377dd90b37a7644a5e55c39365474923c
royayushi <ayushiroy550@gmail.com>
2023-04-23 08:06:32 +0530

    Installed firebase-admin in venv, and added initialized firebase in bus.py

--- bus.py
+++ bus.py
@@ -1,4 +1,20 @@
 from flask import Flask, render_template, request
+import firebase_admin
+from firebase_admin import credentials, db
+
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('path/to/serviceAccountKey.json')
+
+# Initialize the app with a custom auth variable, limiting the server's access
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://findmybus-1bccf-default-rtdb.firebaseio.com/',
+    'databaseAuthVariableOverride': {
+        'uid': 'my-service-worker'
+    }
+})
+
+# Get a database reference
+ref = db.reference('restricted-access/data')
 
 app = Flask(__name__)
 
@@ -6,3 +22,6 @@ app = Flask(__name__)
 @app.route("/home")
 def home_page():
     return render_template('home.html')
+
+
+
